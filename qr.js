53b575f8f211122b8bc0d52c0d0576dc6c58c108
Burak Eyidoğan <78258764+mbesoftware@users.noreply.github.com>
2021-03-02 12:16:04 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -7,20 +7,19 @@ async function mbeWp () {
     const conn = new WAConnection();
     const Session = new StringSession();  
     conn.logger.level = 'warn';
-    conn.regenerateQRIntervalMs = 40000;
+    conn.regenerateQRIntervalMs = 30000;
     
     conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('Mbe')}${chalk.blue.bold('Wp')}
-${chalk.white.italic('MbeString Kodu Alıcı')}
-
-${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
+        console.log(`${chalk.green.bold('Mbe')}${chalk.blue.bold('Whatsapp')}
+${chalk.white.italic('MBE-WP String Kod Alıcı')}
+${chalk.blue.italic('ℹ️  Whatsapp a Bağlanılıyor...')}`);
     });
     
 
     conn.on('open', () => {
         var st = Session.createStringSession(conn.base64EncodedAuthInfo());
         console.log(
-            chalk.green.bold('Mbe String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+            chalk.green.bold('MBE-WOK String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
         );
         
         if (!fs.existsSync('config.env')) {
